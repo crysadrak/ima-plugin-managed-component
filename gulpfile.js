5cd0c76b5a9f16dc380cb1bc7606b96e97c2e8dd
Martin Jurča <mjurca@centrum.cz>
2016-09-07 20:44:54 +0200

    added a better package publication process

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 
+let del = require('del');
 let gulp = require('gulp');
 let babel = require('gulp-babel');
+let runSequence = require('run-sequence');
 
-// build module
-gulp.task('build', () => {
-	return (
-		gulp.src('./src/**/!(*Spec).js')
-			.pipe(babel({
-				moduleIds: true,
-				presets: ['es2015']
-			}))
-			.pipe(gulp.dest('./dist'))
+gulp.task('prepare-package', (done) => {
+	return runSequence(
+		'clean',
+		'build',
+		'copy-metadata',
+		done
 	);
+});
+
+gulp.task('copy-metadata', () => {
+	return gulp.src(['./LICENSE', './package.json', './README.md'])
+		.pipe(gulp.dest('./dist'))
+});
+
+gulp.task('clean', () => {
+	return del([
+		'./dist'
+	]);
+});
+
+gulp.task('build', () => {
+	return gulp.src('./src/**/!(*Spec).js')
+		.pipe(babel({
+			moduleIds: true,
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('./dist'));
 });
