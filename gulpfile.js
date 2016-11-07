8e7b1a9154cdbbdb0cbdac6e669af954aeef7c91
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-11-07 09:21:24 +0100

    switched to gulp 4, updated the build and publish process

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,31 @@
 let del = require('del');
 let gulp = require('gulp');
 let babel = require('gulp-babel');
-let runSequence = require('run-sequence');
 
-gulp.task('prepare-package', (done) => {
-	return runSequence(
-		'clean',
-		'build',
-		'copy-metadata',
-		done
-	);
-});
+exports.build = gulp.series(
+	clean,
+	gulp.parallel(
+		build_js,
+		copy
+	)
+);
 
-gulp.task('copy-metadata', () => {
-	return gulp.src(['./LICENSE', './package.json', './README.md'])
-		.pipe(gulp.dest('./dist'))
-});
-
-gulp.task('clean', () => {
-	return del([
-		'./dist'
-	]);
-});
-
-gulp.task('build', () => {
-	return gulp.src('./src/**/!(*Spec).js')
+function build_js() {
+	return gulp
+		.src('./src/**/!(*Spec).js')
 		.pipe(babel({
 			moduleIds: true,
 			plugins: ['transform-es2015-modules-commonjs']
 		}))
 		.pipe(gulp.dest('./dist'));
-});
+}
+
+function copy() {
+	return gulp
+		.src(['./LICENSE', './package.json', './README.md'])
+		.pipe(gulp.dest('./dist'));
+}
+
+function clean() {
+	return del('./dist');
+}
